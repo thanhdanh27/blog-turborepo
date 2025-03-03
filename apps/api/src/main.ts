46d979073638c2d3341d70895a4a83147f0bcb0d
thanhdanh27 <maithanhdanh112@gmail.com>
2025-03-03 19:52:25 +0700

    fix port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.useLogger(new Logger());
-  await app.listen(8000);
+  await app.listen(process.env.PORT ?? 8000);
 
 }
 bootstrap();
